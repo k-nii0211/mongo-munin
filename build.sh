26f4c146a237af548b622644164eaa632907393d
nii_kenichi <cm10030@gmail.com>
2015-04-22 21:11:55 +0900

    Fix shebang

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-python='';
-if type python26 >/dev/null 2>&1; then
-  python='python26'
-elif type python2.6 >/dev/null 2>&1; then
-  python='python2.6'
-elif type python >/dev/null 2>&1; then
-  python='python'
-else
-  echo 'No known Python found!' >&2
-  exit 10
-fi
-
-python="#!/usr/bin/env $python"
-
 for out in `ls src/body_*.py`
 do
     echo $out
@@ -22,7 +8,7 @@ do
 
     out=mongo_$x
 
-    echo $python > $out
+    echo "#!/usr/bin/env python" > $out
 
     echo "" >> $out
     echo "## GENERATED FILE - DO NOT EDIT" >> $out
